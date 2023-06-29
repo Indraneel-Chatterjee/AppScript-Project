ad2695a5c82e849157d9f7398eaf4d76f727f0f3
INDRANEEL CHATTERJEE <indraneel.chatterjee.24.1@mountblue.tech>
2023-06-30 01:08:37 +0530

    Add rowColumnIntoObject & generateKeys function to Code.js file.

--- src/Code.js
+++ src/Code.js
@@ -1,14 +1,46 @@
 const spreadSheetId = "1hVvCH84KlDiKzyU-aTnvNId7JDAyWzyLeehzCgRNPFo";
+const regex = /[a-zA-Z]/g;
+const sheetDataFull = [];
 
+async function rowColumnIntoObject() {
+  try {
+    const sheet =
+      SpreadsheetApp.openById(spreadSheetId).getSheetByName("Sheet1");
+    const rows = sheet
+      .getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn())
+      .getValues();
 
-function rowColumnIntoObject() {
-  var sheet = SpreadsheetApp.openById(spreadSheetId).getSheetByName("Sheet1")
-  const rows = sheet
-    .getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn())
-    .getValues();
-    const keys= []
-  rows.map((row , index) => {
-    keys.push(row[0])
-  });
-  console.log(keys)
+    const keys = await generateKeys(rows[0]);
+
+    rows.map((row, index) => {
+      const obj = {};
+      if (index > 0) {
+        row.map((elem, index) => {
+          obj[keys[index]] = elem;
+        });
+        sheetDataFull.push(obj);
+      }
+    });
+    console.log(sheetDataFull);
+  } catch (error) {
+    console.log(error, "Assigning Value Failed.");
+  }
+}
+
+async function generateKeys(allColumnHead) {
+  try {
+    const keys = await allColumnHead.map((columnHead) => {
+      columnHead = columnHead.trim();
+      if (columnHead !== "") {
+        const letterArr = columnHead.match(regex);
+        letterArr[0] = letterArr[0].toLowerCase();
+        const key = letterArr.join("");
+        return key;
+      }
+      return columnHead;
+    });
+    return keys;
+  } catch (error) {
+    console.log(error, "Keys Generation Failed.");
+  }
 }
